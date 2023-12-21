ea91e8454710002d7161e5d5fbc934d4dd65a142
StunnaDawg <jonsonallen9@gmail.com>
2023-12-20 22:33:44 -0400

    pokemon go to 1 column for mobile

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,29 +1,55 @@
 import { Link } from "react-router-dom"
 import PokeColumn from "../componentLibrary/PokemonCol"
+import { useEffect, useState } from "react"
 const MainPage = () => {
+  const [isWideScreen, setIsWideScreen] = useState(window.innerWidth >= 1024)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsWideScreen(window.innerWidth >= 1024)
+    }
+
+    window.addEventListener("resize", handleResize)
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  }, [])
   return (
     <>
       <div className="flex flex-row">
-        <PokeColumn
-          pokemon1="pikachu"
-          pokemon2="pichu"
-          pokemon3="luxray"
-          pokemon4="tinkaton"
-        />
+        {isWideScreen ? (
+          <>
+            <PokeColumn
+              pokemon1="pikachu"
+              pokemon2="pichu"
+              pokemon3="luxray"
+              pokemon4="tinkaton"
+            />
 
-        <PokeColumn
-          pokemon1="raichu"
-          pokemon2="diglett"
-          pokemon3="garchomp"
-          pokemon4="charizard"
-        />
+            <PokeColumn
+              pokemon1="raichu"
+              pokemon2="diglett"
+              pokemon3="garchomp"
+              pokemon4="charizard"
+            />
 
-        <PokeColumn
-          pokemon1="charmeleon"
-          pokemon2="mewtwo"
-          pokemon3="mew"
-          pokemon4="phanpy"
-        />
+            <PokeColumn
+              pokemon1="charmeleon"
+              pokemon2="mewtwo"
+              pokemon3="mew"
+              pokemon4="phanpy"
+            />
+          </>
+        ) : (
+          <PokeColumn
+            pokemon1="pikachu"
+            pokemon2="pichu"
+            pokemon3="luxray"
+            pokemon4="tinkaton"
+          />
+        )}
 
         <div className="flex-1 flex flex-col items-center justify-center h-screen">
           <h1 className="font-pokemon-solid underline text-4xl">PokeGuesser</h1>
@@ -42,26 +68,37 @@ const MainPage = () => {
           </Link>
         </div>
 
-        <PokeColumn
-          pokemon1="pikachu"
-          pokemon2="pichu"
-          pokemon3="luxray"
-          pokemon4="tinkaton"
-        />
+        {isWideScreen ? (
+          <>
+            <PokeColumn
+              pokemon1="pikachu"
+              pokemon2="pichu"
+              pokemon3="luxray"
+              pokemon4="tinkaton"
+            />
 
-        <PokeColumn
-          pokemon1="raichu"
-          pokemon2="diglett"
-          pokemon3="garchomp"
-          pokemon4="charizard"
-        />
+            <PokeColumn
+              pokemon1="raichu"
+              pokemon2="diglett"
+              pokemon3="garchomp"
+              pokemon4="charizard"
+            />
 
-        <PokeColumn
-          pokemon1="charmeleon"
-          pokemon2="mewtwo"
-          pokemon3="mew"
-          pokemon4="phanpy"
-        />
+            <PokeColumn
+              pokemon1="charmeleon"
+              pokemon2="mewtwo"
+              pokemon3="mew"
+              pokemon4="phanpy"
+            />
+          </>
+        ) : (
+          <PokeColumn
+            pokemon1="pikachu"
+            pokemon2="pichu"
+            pokemon3="luxray"
+            pokemon4="tinkaton"
+          />
+        )}
       </div>
     </>
   )
