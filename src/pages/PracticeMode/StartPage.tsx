3d745f0e536453ec12e8c31b0327e6b7ceee8b0c
StunnaDawg <jonsonallen9@gmail.com>
2024-01-17 20:12:58 -0400

    styling fixes

--- src/pages/PracticeMode/StartPage.tsx
+++ src/pages/PracticeMode/StartPage.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { Link, Outlet } from "react-router-dom"
 import { useCategoryContext } from "../../context"
+import GenChoose from "../../componentLibrary/GenChoose"
 
 const ChoosePracticeMode = () => {
   const [clickedLink, setClickedLink] = useState<boolean>(false)
@@ -9,197 +10,135 @@ const ChoosePracticeMode = () => {
     <>
       {!clickedLink ? (
         <>
-          <div className="flex flex-col items-center h-screen">
-            <div className="flex flex-row justify-center">
-              <div className="mx-2 text-center font-pokemon-solid">
-                <h1 className="text-3xl font-pokemon-solid">Practice Mode</h1>
+          <div className="flex flex-row justify-center items-center mt-8">
+            <div className="flex flex-col items-center h-screen">
+              <h1 className="text-5xl font-pokemon-solid underline mt-10">
+                Welcome to Practice Mode
+              </h1>
+              <div className="flex flex-row justify-center">
+                <div className="text-center items-center ">
+                  <GenChoose
+                    setCategoryEnd={setCategoryEnd}
+                    setCategoryStart={setCategoryStart}
+                    setClickedLink={setClickedLink}
+                    categoryStart={0}
+                    categoryEnd={1015}
+                    imgLink="https://img.pokemondb.net/sprites/scarlet-violet/normal/ditto.png"
+                    imgAlt="Ditto"
+                    title="All Gens"
+                    linkToName="all-gens"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-row justify-center m-5">
+                <GenChoose
+                  setCategoryEnd={setCategoryEnd}
+                  setCategoryStart={setCategoryStart}
+                  setClickedLink={setClickedLink}
+                  categoryStart={0}
+                  categoryEnd={151}
+                  imgLink="https://img.pokemondb.net/sprites/scarlet-violet/normal/raichu.png"
+                  imgAlt="Raichu"
+                  title="Gen 1"
+                  linkToName="gen-1"
+                />
+                <GenChoose
+                  setCategoryEnd={setCategoryEnd}
+                  setCategoryStart={setCategoryStart}
+                  setClickedLink={setClickedLink}
+                  categoryStart={152}
+                  categoryEnd={251}
+                  imgLink="https://img.pokemondb.net/sprites/scarlet-violet/normal/donphan.png"
+                  imgAlt="Donphan"
+                  title="Gen 2"
+                  linkToName="gen-2"
+                />
+                <GenChoose
+                  setCategoryEnd={setCategoryEnd}
+                  setCategoryStart={setCategoryStart}
+                  setClickedLink={setClickedLink}
+                  categoryStart={252}
+                  categoryEnd={386}
+                  imgLink="https://img.pokemondb.net/sprites/scarlet-violet/normal/swampert.png"
+                  imgAlt="swampert"
+                  title="Gen 3"
+                  linkToName="gen-3"
+                />
+              </div>
+              <div className="flex flex-row justify-center m-5">
+                <GenChoose
+                  setCategoryEnd={setCategoryEnd}
+                  setCategoryStart={setCategoryStart}
+                  setClickedLink={setClickedLink}
+                  categoryStart={387}
+                  categoryEnd={493}
+                  imgLink="https://img.pokemondb.net/sprites/scarlet-violet/normal/shinx.png"
+                  imgAlt="shinx"
+                  title="Gen 4"
+                  linkToName="gen-4"
+                />
 
-                <Link
-                  to="all-gens"
-                  className="text-2xl"
-                  onClick={() => {
-                    setCategoryStart(0),
-                      setCategoryEnd(1015),
-                      setClickedLink(true)
-                  }}
-                >
-                  <img
-                    src="https://img.pokemondb.net/sprites/scarlet-violet/normal/mew.png"
-                    alt="Mew"
-                    className="w-24 h-24 object-contain m-4"
+                <div className="flex flex-row justify-center">
+                  <GenChoose
+                    setCategoryEnd={setCategoryEnd}
+                    setCategoryStart={setCategoryStart}
+                    setClickedLink={setClickedLink}
+                    categoryStart={494}
+                    categoryEnd={649}
+                    imgLink="https://img.pokemondb.net/sprites/scarlet-violet/normal/axew.png"
+                    imgAlt="Axew"
+                    title="Gen 5"
+                    linkToName="gen-5"
                   />
-                  All Generations
-                </Link>
+                </div>
+                <GenChoose
+                  setCategoryEnd={setCategoryEnd}
+                  setCategoryStart={setCategoryStart}
+                  setClickedLink={setClickedLink}
+                  categoryStart={650}
+                  categoryEnd={721}
+                  imgLink="https://img.pokemondb.net/sprites/scarlet-violet/normal/greninja.png"
+                  imgAlt="Greninja"
+                  title="Gen 6"
+                  linkToName="gen-6"
+                />
               </div>
-            </div>
 
-            <div className="flex flex-row justify-center">
-              <div className="mx-2 text-center font-pokemon-solid">
-                <Link
-                  to="gen-1"
-                  className="text-2xl"
-                  onClick={() => {
-                    setCategoryStart(0),
-                      setCategoryEnd(151),
-                      setClickedLink(true)
-                  }}
-                >
-                  <img
-                    src="https://img.pokemondb.net/sprites/scarlet-violet/normal/flareon.png"
-                    alt="Flareon"
-                    className="w-24 h-24 object-contain m-4"
-                  />
-                  Gen 1
-                </Link>
-              </div>
-              <div className="mx-2 text-center font-pokemon-solid">
-                <Link
-                  to="gen-2"
-                  className="text-2xl"
-                  onClick={() => {
-                    setCategoryStart(152),
-                      setCategoryEnd(251),
-                      setClickedLink(true)
-                  }}
-                >
-                  <img
-                    src="https://img.pokemondb.net/sprites/scarlet-violet/normal/quagsire.png"
-                    alt="Quagsire"
-                    className="w-24 h-24 object-contain m-4"
-                  />
-                  Gen 2
-                </Link>
-              </div>
-              <div className="mx-2 text-center font-pokemon-solid">
-                <Link
-                  to="gen-3"
-                  className="text-2xl"
-                  onClick={() => {
-                    setCategoryStart(252),
-                      setCategoryEnd(386),
-                      setClickedLink(true)
-                  }}
-                >
-                  <img
-                    src="https://img.pokemondb.net/sprites/scarlet-violet/normal/medicham.png"
-                    alt="Medicham"
-                    className="w-24 h-24 object-contain m-4"
-                  />
-                  Gen 3
-                </Link>
-              </div>
-            </div>
-            <div className="flex flex-row justify-center">
-              <div className="mx-2 text-center font-pokemon-solid">
-                <Link
-                  to="gen-4"
-                  className="text-2xl"
-                  onClick={() => {
-                    setCategoryStart(387),
-                      setCategoryEnd(493),
-                      setClickedLink(true)
-                  }}
-                >
-                  <img
-                    src="https://img.pokemondb.net/sprites/scarlet-violet/normal/mesprit.png"
-                    alt="Mesprit"
-                    className="w-24 h-24 object-contain m-4"
-                  />
-                  Gen 4
-                </Link>
-              </div>
-              <div className="mx-2 text-center font-pokemon-solid">
-                <Link
-                  to="gen-5"
-                  className="text-2xl"
-                  onClick={() => {
-                    setCategoryStart(494),
-                      setCategoryEnd(649),
-                      setClickedLink(true)
-                  }}
-                >
-                  <img
-                    src="https://img.pokemondb.net/sprites/scarlet-violet/normal/zoroark.png"
-                    alt="Zoroark"
-                    className="w-24 h-24 object-contain m-4"
-                  />
-                  Gen 5
-                </Link>
-              </div>
-              <div className="mx-2 text-center font-pokemon-solid">
-                <Link
-                  to="gen-6"
-                  className="text-2xl"
-                  onClick={() => {
-                    setCategoryStart(650),
-                      setCategoryEnd(721),
-                      setClickedLink(true)
-                  }}
-                >
-                  <img
-                    src="https://img.pokemondb.net/sprites/scarlet-violet/normal/goodra.png"
-                    alt="Goodra"
-                    className="w-24 h-24 object-contain m-4"
-                  />
-                  Gen 6
-                </Link>
-              </div>
-            </div>
+              <div className="flex flex-row justify-center m-5">
+                <GenChoose
+                  setCategoryEnd={setCategoryEnd}
+                  setCategoryStart={setCategoryStart}
+                  setClickedLink={setClickedLink}
+                  categoryStart={722}
+                  categoryEnd={809}
+                  imgLink="https://img.pokemondb.net/sprites/scarlet-violet/normal/rowlet.png"
+                  imgAlt="Rowlet"
+                  title="Gen 7"
+                  linkToName="gen-7"
+                />
 
-            <div className="flex flex-row justify-center">
-              <div className="mx-2 text-center font-pokemon-solid">
-                <Link
-                  to="gen-7"
-                  className="text-2xl"
-                  onClick={() => {
-                    setCategoryStart(722),
-                      setCategoryEnd(809),
-                      setClickedLink(true)
-                  }}
-                >
-                  <img
-                    src="https://img.pokemondb.net/sprites/scarlet-violet/normal/mudbray.png"
-                    alt="Mudbray"
-                    className="w-24 h-24 object-contain m-4"
-                  />
-                  Gen 7
-                </Link>
-              </div>
-              <div className="mx-2 text-center font-pokemon-solid">
-                <Link
-                  to="gen-8"
-                  className="text-2xl"
-                  onClick={() => {
-                    setCategoryStart(810),
-                      setCategoryEnd(905),
-                      setClickedLink(true)
-                  }}
-                >
-                  <img
-                    src="https://img.pokemondb.net/sprites/scarlet-violet/normal/toxtricity.png"
-                    alt="Toxtricity"
-                    className="w-24 h-24 object-contain m-4"
-                  />
-                  Gen 8
-                </Link>
-              </div>
-              <div className="mx-2 text-center font-pokemon-solid">
-                <Link
-                  to="gen-9"
-                  className="text-2xl"
-                  onClick={() => {
-                    setCategoryStart(906),
-                      setCategoryEnd(1010),
-                      setClickedLink(true)
-                  }}
-                >
-                  <img
-                    src="https://img.pokemondb.net/sprites/scarlet-violet/normal/bellibolt.png"
-                    alt="Bellibolt"
-                    className="w-24 h-24 object-contain m-4"
-                  />
-                  Gen 9
-                </Link>
+                <GenChoose
+                  setCategoryEnd={setCategoryEnd}
+                  setCategoryStart={setCategoryStart}
+                  setClickedLink={setClickedLink}
+                  categoryStart={810}
+                  categoryEnd={905}
+                  imgLink="https://img.pokemondb.net/sprites/scarlet-violet/normal/snom.png"
+                  imgAlt="Snom"
+                  title="Gen 8"
+                  linkToName="gen-8"
+                />
+                <GenChoose
+                  setCategoryEnd={setCategoryEnd}
+                  setCategoryStart={setCategoryStart}
+                  setClickedLink={setClickedLink}
+                  categoryStart={906}
+                  categoryEnd={1010}
+                  imgLink="https://img.pokemondb.net/sprites/scarlet-violet/normal/tinkaton.png"
+                  imgAlt="Tinkaton"
+                  title="Gen 9"
+                  linkToName="gen-9"
+                />
               </div>
             </div>
           </div>
