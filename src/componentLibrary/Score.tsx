b117b0e40b9a1071a086fe7946cea80425873067
StunnaDawg <jonsonallen9@gmail.com>
2024-01-22 20:52:48 -0400

    fixed unused variables

--- src/componentLibrary/Score.tsx
+++ src/componentLibrary/Score.tsx
@@ -1,11 +1,9 @@
-import React from "react";
-import { useUserScore } from "../context";
+import React from "react"
+import { useUserScore } from "../context"
 
-const UserScore: React.FC = () => { 
-    const {userScore, setUserScore} = useUserScore()
- return (
-    <h1>Score: {userScore}</h1>
- )
+const UserScore: React.FC = () => {
+  const { userScore } = useUserScore()
+  return <h1>Score: {userScore}</h1>
 }
 
 export default UserScore
