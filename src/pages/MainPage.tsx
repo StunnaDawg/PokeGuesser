ceadacd6095fb5fbe1bca470bacef05aeb8a52fe
StunnaDawg <jonsonallen9@gmail.com>
2024-02-12 11:30:41 -0400

    fixed some styling

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -46,25 +46,25 @@ const MainPage = () => {
           )}
         </div>
         <div className="flex flex-col items-center justify-center h-screen">
-          <h1 className="font-pokemon-solid underline text-3xl xl:text-5xl 2xl:text-7xl">
+          <h1 className="font-pokemon-solid underline text-3xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
             PokeGuesser
           </h1>
 
           <Link
             to="/classic"
-            className="my-2 font-pokemon-solid text-2xl xl:text-4xl 2xl:text-5xl hover:text-blue-800"
+            className="my-2 font-pokemon-solid text-2xl lg:text-4xl xl:text-5xl 2xl:text-5xl hover:text-blue-800"
           >
             Classic Mode
           </Link>
           <Link
             to="/practice-mode"
-            className="my-2 font-pokemon-solid text-2xl xl:text-3xl 2xl:text-4xl hover:text-blue-800"
+            className="my-2 font-pokemon-solid text-2xl lg:text-3xl xl:text-4xl 2xl:text-4xl hover:text-blue-800"
           >
             Practice Mode
           </Link>
           <Link
             to="/leaderboard"
-            className="my-2 font-pokemon-solid text-2xl xl:text-3xl 2xl:text-4xl hover:text-blue-800"
+            className="my-2 font-pokemon-solid text-2xl lg:text-3xl xl:text-4xl 2xl:text-4xl hover:text-blue-800"
           >
             LeaderBoard
           </Link>
